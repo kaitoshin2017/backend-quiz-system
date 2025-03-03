c6a9471a5b150bbb06de0def23ed07c62cac8cd1
kaitoshin2017 <kboom8301@gmail.com>
2025-03-04 01:16:11 +0800

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const UserController = require('../Controller/UserController');
-
-router.get('/users', UserController.getUsers);
-
-
-module.exports = router; 
