909f8e172023f796a14aa6092e68c224c3965953
kaitoshin2017 <kboom8301@gmail.com>
2025-03-20 16:37:36 +0800

    hgjygyhhh

--- express-mvc-ict-main/routes/studentRoutes.js
+++ express-mvc-ict-main/routes/studentRoutes.js
@@ -1,10 +1,10 @@
-
+// studentRoutes.js
 const express = require('express');
 const { registerStudent, loginStudent } = require('../controllers/studentController');
 
 const router = express.Router();
 
 router.post('/register', registerStudent);
-router.post('/login', loginStudent); 
+router.post('/login', loginStudent); // Adding login route
 
 module.exports = router;
